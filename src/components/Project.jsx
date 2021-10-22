695e3b43f521b0ed204c7c2c3c17304fd343c48e
OrlandoMts <dani_monts19@hotmail.com>
2021-10-21 22:56:08 -0500

    instale la dependencia de ghpages

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,7 +9,7 @@ function Project(props) {
             <li className="li__projectsList--desc"><p>{props.desc}</p></li>
             <div className="projectsList__links">
                 <li className="li__link"><a href={props.sitio} target="_blank" rel="noreferrer">Sitio</a></li>
-                <li className="li__link"><a href={props.repo} target="_blank" rel="noreferrer">Repositorio</a></li>
+                <li className="li__link"><a href={props.repo} target="_blank" rel="noreferrer">Ver codigo</a></li>
             </div>
         </div>
     )
